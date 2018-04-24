b9a74e648f0113db60b87fdd63d26b700d9b8f0a
Dries Cornilly <dries.cornilly@telenet.be>
2018-04-24 18:28:17 +0200

    speedup bivariate logistic sampling

--- src/levy_bivlog.cpp
+++ src/levy_bivlog.cpp
@@ -12,11 +12,13 @@ arma::mat rjump_BIVLOG(int n, double p1, double p2) {
   
   double delta1 = log(1.0 - p2) / log(1.0 - p1 - p2);
   arma::vec bin = rbinom(n, 1, 1.0 - delta1);
+  
+  arma::vec rj_log = rjump_NEGBIN(n, p2);
 
   for (int ii = 0; ii < n; ii++) {
     if (bin(ii) < 0.5) {
       rj(ii, 0) = 0.0;
-      rj(ii, 1) = rjump_NEGBIN(n, p2)(0);
+      rj(ii, 1) = rj_log(ii);
     } else {
       rj(ii, 1) = rnbinom(1, rj(ii, 0), 1.0 - p2)(0);
     }

6d45568a4751fc0b0ce937ceb77af69c11d7676f
Dries Cornilly <dries.cornilly@telenet.be>
2018-05-18 09:06:00 -0500

    default settings for simulation

--- R/sim_trawl.R
+++ R/sim_trawl.R
@@ -29,22 +29,25 @@
 sim_trawl <- function(object, univariate = TRUE, ...) {
   
   # extract settings
-  levy_seed <- object$levy_seed
-  levy_par <- object$levy_par
-  trawl <- object$trawl
-  trawl_par <- object$trawl_par
-  T0 <- object$T0
-  TT <- object$TT
-  observed_freq <- object$observed_freq
-  b <- object$b
+  nn <- names(object)
+  if ("levy_seed" %in% nn) levy_seed <- object$levy_seed else levy_seed <- "Poisson"
+  if ("levy_par" %in% nn) levy_par <- object$levy_par else levy_par <- 0.0131
+  if ("trawl" %in% nn) trawl <- object$trawl else trawl <- "exp"
+  if ("trawl" %in% nn) trawl_par <- object$trawl_par else trawl_par <- trawl_x0(trawl)
+  if ("T0" %in% nn) T0 <- object$T0 else T0 <- 0
+  if ("TT" %in% n) TT <- object$TT else TT <- 3600
+  if ("observed_freq" %in% nn) observed_freq <- object$observed_freq else observed_freq <- 1e-3
+  if ("b" %in% n) b <- object$b else b <- 0
+  
   
   # simulate
   if (univariate) {
     lsim <- simulate_trawl_uv(levy_seed, as.numeric(levy_par), trawl, as.numeric(trawl_par), 
                               as.numeric(T0), as.numeric(TT), as.numeric(observed_freq), as.numeric(b))
   } else {
+    if ("design_matrix" %in% nn) design_matrix <- object$design_matrix else design_matrix <- diag(length(trawl))
+    
     if (levy_seed %in% c("Poisson", "Skellam")) {
-      design_matrix <- object$design_matrix
       lsim <- simulate_trawl_mv_Poisson(levy_par, trawl, trawl_par, design_matrix, as.numeric(T0), 
                                         as.numeric(TT), as.numeric(observed_freq), as.numeric(b))
     } else if (levy_seed == "negBin") {

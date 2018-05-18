1cb6abf125a4b7a9d19e1c1664b1bf424955b935
cdries <dries.cornilly@telenet.be>
2018-05-18 13:00:02 -0500

    documentation vs_trawl

--- R/vs_trawl.R
+++ R/vs_trawl.R
@@ -1,27 +1,54 @@
-#' Estimation of Trawl processes
+#' Variance signature values
 #'
-#' display variance signature plot
+#' compute empirical and theoretical variance signature values
 #'
-#' TODO
+#' the object is either an output object from sim_trawl for the empirical variance signature
+#' values, or an output object from fit_trawl for the theoretical ones. When computing the sample
+#' variance signature plot, it is possible to add the argument 'multi' for reducing the 
+#' estimation variance. See Shephard and Yang (2017) for a definition of the variance 
+#' signature values.
 #'
-#' CITE TODO.
 #' @name vs_trawl
 #' @concept trawl
-#' @param object bla
-#' @param h bla
-#' @param method bla
+#' @param object object containing all the specifications for the process, see details
+#' @param h vector containing the observation frequencies at 
+#' which to compute the variance signature values
+#' @param method "sample" for the empirical values, "vs_C" or "vs_SY" for theoretical values when
+#' estimated by these respective mtehods with fit_trawl
 #' @param \dots any other passthru pareters
 #' @author Dries Cornilly
-#' @seealso \code{\link{fit_trawl}}
+#' @seealso \code{\link{fit_trawl}}, \code{\link{sim_trawl}}
 #' @references
-#' TODO
+#' Shephard, N., & Yang, J. J. (2017). 
+#' Continuous time analysis of fleeting discrete price moves. 
+#' Journal of the American Statistical Association, 112(519), 1090-1106.
 #'
 #' @examples
+#' 
+#' # simulate a trawl process
+#' sim <- sim_trawl(list("levy_seed" = "Skellam", levy_par = c(0.13, 0.11), b = 0.3))
+#' h <- exp(seq(log(1e-2), log(60), length.out = 51))
 #'
-#' #TODO
+#' # empirical variance signature values
+#' vsS <- vs_trawl(sim, h)
 #'
-#' # simulations estimation
-#' #TODO
+#' # theoretical values when estimated with "vs_C"
+#' sim$method <- "vs_C"     # add fitting method
+#' sim$h <- h               # add grid on which to fit
+#' ft <- fit_trawl(sim)     # fit the trawl process
+#' vsC <- vs_trawl(ft, h, method = "vs_C")  # compute fitted variance signature values
+#'
+#' # theoretical values when estimated with "vs_SY"
+#' sim$method <- "vs_SY"    # add fitting method
+#' sim$h <- h               # add grid on which to fit
+#' ft <- fit_trawl(sim)     # fit the trawl process
+#' vsSY <- vs_trawl(ft, h, method = "vs_SY")  # compute fitted variance signature values
+#' 
+#' # plot resulting fits
+#' plot(log(h), vsS)
+#' lines(log(h), vsC, col = "blue")
+#' lines(log(h), vsSY, col = "red")
+#' legend("topright", c("vs_C", "vs_SY"), lwd = c(2, 2), col = c("blue", "red"))
 #'
 #' @export vs_trawl
 vs_trawl <- function(object, h, method = "sample", ...) {
@@ -44,10 +71,8 @@ vs_trawl <- function(object, h, method = "sample", ...) {
                 as.numeric(object$b), FALSE)$vs_theor
 
   } else {
-
+    stop("provide a valid method.")
   }
-
-
 
   return(vs)
 }

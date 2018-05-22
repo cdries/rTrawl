3ddac601effeffa4a071abd9825cde6bc25c0fde
cdries <dries.cornilly@telenet.be>
2018-05-22 13:16:05 -0500

    add price data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Trade prices in ES
+#'
+#' A dataset containing the trade prices for the ES contract with expiration
+#' M8 on May 7, 2018 between 8h30 and 12h00 CDT.
+#'
+#' @format An xts object with 1 variable
+#' \describe{
+#'   \item{Price}{trade price at which a trade occurred, in USD}
+#' }
+#' @source Brian G. Peterson
+"ESM8"
